85656908ed6975bdaf833d5101b97d62bf45f773
S. Jansen <sa.jansen1@gmail.com>
2021-10-02 12:26:33 +0200

    feat(game): Added data for Game Of Life

--- src/scripts/games/gameOfLife.tsx
+++ src/scripts/games/gameOfLife.tsx
@@ -1,30 +1,18 @@
 import game, {GameInterface} from "./game";
 import {Deck} from "../objects/Deck";
 import {Card} from "../objects/Card";
+import data from "../../assets/games/gameOfLifeDateEdition.json";
 
 export class GameOfLife implements GameInterface {
     start() {
-        const kleineActies = new Deck("Kleine acties");
-        kleineActies.add(new Card("Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1"))
-        kleineActies.add(new Card("Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2"))
-        for(let i = 0; i < 1; i++) {
-            kleineActies.add(new Card("Kleine actie no. 3"));
-        }
-
-        kleineActies.shuffle();
-
-        const groteActies = new Deck("Grote acties");
-        groteActies.add(new Card("Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1Kleine actie no. 1"))
-        groteActies.add(new Card("Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2Kleine actie no. 2"))
-        for(let i = 0; i < 10; i++) {
-            groteActies.add(new Card("Kleine actie no. 3"));
-        }
-
-        groteActies.shuffle();
-
         game.clear();
         game.setGame(this);
-        game.addDeck(kleineActies);
-        game.addDeck(groteActies);
+
+        Object.entries(data).forEach(([name, cards]) => {
+            const deck = new Deck(name);
+            cards.forEach((card) => deck.add(new Card(card)));
+            deck.shuffle();
+            game.addDeck(deck);
+        });
     }
 }
